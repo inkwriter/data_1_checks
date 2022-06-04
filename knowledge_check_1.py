a0a76b41c817f1a54eebc2da7a9e449347d49df1
Jake Sexton <drained77@gmail.com>
2022-06-03 20:04:59 -0400

    First commit

---
+++ knowledge_check_1.py
@@ -0,0 +1,13 @@
+print("hello world!")
+
+#The list
+list = ["hello", "world", "friends"]
+print(list)
+
+#The dictionary
+dictionary = {"movie1": "description1", "movie2": "descripition"}
+print(dictionary["movie1"])
+
+#Tuple
+the_tuple = ("apple", "orange", "pineapple")
+print(the_tuple[0])
